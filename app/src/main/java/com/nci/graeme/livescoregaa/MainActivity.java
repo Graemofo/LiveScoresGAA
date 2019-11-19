51de7debf75a32bc2b33ec6c94c5ce0f0a16071e
Graeme <graeme.doherty84@gmail.com>
2019-11-19 16:06:48 +0000

    Initial commit for LiveScoreGaa app

---
+++ app/src/main/java/com/nci/graeme/livescoregaa/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nci.graeme.livescoregaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }//end of on create
+}//end of MainActivity
